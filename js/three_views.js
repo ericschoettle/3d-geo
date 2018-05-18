00c705868e315aa830ff0bc4b063f7b7bb9bc78b
ericschoettle <eric.schoettle@gmail.com>
2018-05-17 17:03:56 -0700

    Refactor points of intersecdtion function into while loop, move to face object

--- js/three_views.js
+++ js/three_views.js
@@ -75,8 +75,6 @@ for (let i = 0; i < numberOfBeds; i++) {
   beds.add( bed )
 }
 
-
-
 class ClipCube {
   constructor(edgeLength) {
     //Edge length of 2 gives faces at 1, -1, respectively, with cube centered on (0,0,0,)
@@ -114,12 +112,39 @@ class ClipCube {
 
   // Clip planes define a cube, for which only things on the inside are visible. 
   makeFaces() {
-    let faces = {};
+    let faces = [];
     for (const [faceType, normalVector] of Object.entries(this.normalVectors)) {
       // Plane goes to infinity in all directions. Position, in direction of normal vectork, is half of edge length
-      faces[faceType] = new Face(faceType, normalVector);
+      faces.push(new Face(faceType, normalVector));
     }
     return faces;
+  }
+
+
+  coverFaces () {
+    this.faces.forEach((face) => {
+
+      /** Here I need logic to see if I'm at first or last bed that intersects (if previous or next bed doesn't intersect).
+       * I need to flip the sequence to ['top', 'bottom', 'bottom', top'] if there are corners that need to be grabbed below the bed rather than above
+       * I also need to figure out how to color if ZERO point intersect. Tricky, because relevant information shows up on other faces. 
+       */
+
+      // TODO pull out, make object oriented.
+      let results = {}
+      beds.children.forEach(bed => {
+        let planes = {
+          top: planeFromObject(bed, 4), // 4 and 5 are the indicies of the triangles on the top face of the bed (BoxGeometry) made by THREE.js
+          bottom: planeFromObject(bed, 6), // 6 and 7 are the indicies of the triangles on the bottom face of the bed (BoxGeometry) made by THREE.js
+        }
+        
+        results = face.findPointsOfIntersection(bed, planes);
+       
+      });
+      if (results.sequenceCounter !== 4) {
+        console.log(`WARNING!! Only ${results.sequenceCounter} corners found on face: `, face) 
+      }
+      
+    });
   }
 }
 
@@ -128,8 +153,8 @@ class Face {
     this.faceType = faceType;
     this.normalVector = normalVector;
 
-    if (!this.verticies) {
-      this.verticies = this.makeVerticies();
+    if (!this.vertices) {
+      this.vertices = this.makeVertices();
     }
 
     if (!this.edges) {
@@ -140,15 +165,15 @@ class Face {
 
   makeEdges() {
     let edges = []
-    for (let i = 0; i < this.verticies.length -1; i++) {
-      edges.push(new THREE.Line3(this.verticies[i], this.verticies[i + 1]))
+    for (let i = 0; i < this.vertices.length -1; i++) {
+      edges.push(new THREE.Line3(this.vertices[i], this.vertices[i + 1]))
     }
     return edges;
   }
 
-  makeVerticies() {
-    console.log('making verticies')
-    let verticies = []
+  makeVertices() {
+    // console.log('making vertices')
+    let vertices = []
 
     let firstZeroKey = '';
     let secondZeroKey = '';
@@ -162,7 +187,7 @@ class Face {
     };
 
     // find zero direction, designate other two so we can work with them in predicatable order
-    'xyz'.split().forEach((letter) => {
+    'xyz'.split('').forEach((letter) => {
       if (this.normalVector[letter]) {
         fixedKey = letter;
         fixedVal = this.normalVector[letter];
@@ -175,98 +200,86 @@ class Face {
       }
     })
 
-    // Cycle around face (clockwise), so that verticies are in order and share a vertex with previous and next vertex. 
+    // Cycle around face (clockwise), so that vertices are in order and share a vertex with previous and next vertex. 
     currentVertex[fixedKey] = fixedVal;
-    verticies.push(new THREE.Vector3(currentVertex.x, currentVertex.y, currentVertex.z));
+    vertices.push(new THREE.Vector3(currentVertex.x, currentVertex.y, currentVertex.z));
     currentVertex[secondZeroKey] = 1;
-    verticies.push(new THREE.Vector3(currentVertex.x, currentVertex.y, currentVertex.z));
+    vertices.push(new THREE.Vector3(currentVertex.x, currentVertex.y, currentVertex.z));
     currentVertex[firstZeroKey] = 1;
-    verticies.push(new THREE.Vector3(currentVertex.x, currentVertex.y, currentVertex.z));
+    vertices.push(new THREE.Vector3(currentVertex.x, currentVertex.y, currentVertex.z));
     currentVertex[secondZeroKey] = -1;
-    verticies.push(new THREE.Vector3(currentVertex.x, currentVertex.y, currentVertex.z));
+    vertices.push(new THREE.Vector3(currentVertex.x, currentVertex.y, currentVertex.z));
     currentVertex[firstZeroKey] = -1;
-    verticies.push(new THREE.Vector3(currentVertex.x, currentVertex.y, currentVertex.z)); // Fifth vertex is same as start - makes it easier to build edges by giong in a loop
-    return verticies
+    vertices.push(new THREE.Vector3(currentVertex.x, currentVertex.y, currentVertex.z)); // Fifth vertex is same as start - makes it easier to build edges by giong in a loop
+    return vertices
   }
 
-  // makeFaces() {
-  //   return normalVectors.keys();
-  // } 
+  // Recursive function to find where a bed intersects with the edges of a face.
+  findPointsOfIntersection(bed, planes) {
 
-  // Do I want this on the Face or ClipCube class?
-  coverFaces () {
-    clipCube.faces.forEach((face) => {
-      let planes = {
-        top: planeFromObject(bed, 4), // 4 and 5 are the indicies of the triangles on the top face of the bed (BoxGeometry) made by THREE.js
-        bottom: planeFromObject(bed, 6), // 6 and 7 are the indicies of the triangles on the bottom face of the bed (BoxGeometry) made by THREE.js
-      }
-      /** Here I need logic to see if I'm at first or last bed that intersects (if previous or next bed doesn't intersect).
-       * I need to flip the sequence to ['top', 'bottom', 'bottom', top'] if there are corners that need to be grabbed below the bed rather than above
-       * I also need to figure out how to color if ZERO point intersect. Tricky, because relevant information shows up on other faces. 
-       */
+    let points = [];
+    let pointSequence = ['bottom', 'top', 'top', 'bottom'];
+    let sequenceCounter = 0;
+    let edgeCounter = 0;
+    let edges = this.edges;
 
-      // TODO pull out, make object oriented. 
-      beds.children.forEach(bed => {
-        findPointsOfIntersection([], ['bottom', 'top', 'top', 'bottom'], 0, 0)
-        function findPointsOfIntersection(points, pointSequence, sequenceID, currentEdgeID) {
-          // No points found
-          if (sequenceID === 0 && currentEdgeID === 4) {
-            return 'false';
-          }
-          // Add points and advance
-          let point = planes[pointSequence[sequenceID]].intersectLine(face.edge[currentEdgeID]);
-          if (point) {
-            points.push(point)
-            sequenceID++
-          };
-          // If haven't found, skip to next edge. 
-          if (sequenceID === 0) {
-            findPointsOfIntersection(points, pointSequence, sequenceID, currentEdgeID++);
-          }
-          // If point, look for next one point on same edge, otherwise, add a corner
-          else if (sequenceID === 1) { // this needs to have a check for if 
-            if (point) {
-              secondPointOnEdge(points, pointSequence, sequenceID, currentEdgeID);
-            } else {
-              let end = planes[pointSequence[4]].intersectLine(face.edge[currentEdgeID]);
-              if (end) {
-                points.push(point)
-                return points
-              } else {
-                addCorner(points, pointSequence, sequenceID, currentEdgeID);
-              }
-            }
-          }
-          // on 'outside' of color, skip to next edge
-          else if (sequenceID === 2) {
-            findPointsOfIntersection(points, pointSequence, sequenceID, currentEdgeID++);
-          // on 'inside' of color
-          } else if (sequenceID === 3) {
-            secondPointOnEdge(points, pointSequence, sequenceID, currentEdgeID++);
-          } else { // sequenceID === 4
-            return points;
-          }
-        }
-        function secondPointOnEdge(points, pointSequence, sequenceID, currentEdgeID) {
-          // Add points and advance
-          let point = planes[pointSequence[sequenceID]].intersectLine(face.edge[currentEdgeID]);
-          if (point) {
-            points.push(point)
-            findPointsOfIntersection(points, pointSequence, sequenceID++, currentEdgeID++);
+    while ((sequenceCounter < 4) && (edgeCounter < 4)) {
+      // Add points and advance
+      let point = planes[pointSequence[sequenceCounter]].intersectLine(edges[edgeCounter]);
+      if (point) {
+        points.push(point);
+        sequenceCounter++;
+      };
+
+      // If haven't found, skip to next edge. 
+      if (sequenceCounter === 0) {
+        edgeCounter++;
+      // If point, look for next one point on same edge, otherwise, add a corner
+      } else if (sequenceCounter === 1) { 
+        if (point) {
+          secondPointOnEdge()
+        // Check for end or add corner.
+        } else {
+          let end = planes[pointSequence[3]].intersectLine(edges[edgeCounter]);
+          if (end) {
+            points.push(end)
+            break
           } else {
-            addCorner(points, pointSequence, sequenceID, currentEdgeID);
+            addCorner();
           }
         }
-        function addCorner(points, pointSequence, sequenceID, currentEdgeID) {
+      // on 'outside' of color, skip to next edge
+      } else if (sequenceCounter === 2) {
+        edgeCounter++;
+      // on 'inside' of color
+      } else if (sequenceCounter === 3) {
+        secondPointOnEdge()
+      }
+    }
 
-          findPointsOfIntersection(points, pointSequence, sequenceID, currentEdgeID++);
-        }
-      });
-    });
+    function secondPointOnEdge() {
+      // Add points and advance
+      let point = planes[pointSequence[sequenceCounter]].intersectLine(edges[edgeCounter]);
+      if (point) {
+        // console.log('SECOND POINT ON EDGE', 'point: ', point, 'sequenceCounter: ', sequenceCounter, 'plane:', pointSequence[sequenceCounter], 'edgeCounter: ', edgeCounter);
+        points.push(point)
+        sequenceCounter++;
+        edgeCounter++;
+      } else {
+        addCorner();
+      }
+    }
+  
+    function addCorner() {
+      points.push(edges[edgeCounter].end);
+      edgeCounter++;
+    }
+  return {points: points, sequenceCounter: sequenceCounter}
   }
 }
 
 let clipCube = new ClipCube
+
 
 init();
 animate();
@@ -304,6 +317,8 @@ function init() {
   beds.rotation.x = Math.PI/180 * dip
   beds.rotation.order = "YXZ"
   scene.add(beds)
+
+  clipCube.coverFaces();
 
   // todo: find a good place for this stuff
   renderer = new THREE.WebGLRenderer( { antialias: true, localClippingEnabled: true } );
@@ -409,7 +424,7 @@ folderLocal.add( props, 'Dip', 0, 90 );
 
 
 
-//   if (yAxisBoundingLines.length === 0) {makeLinesFromVerticies(); }
+//   if (yAxisBoundingLines.length === 0) {makeLinesFromVertices(); }
 //   //Loop through each bed
 //   for (let i = 0; i < beds.children.length; i++) {
 //     //Set variables
@@ -426,24 +441,24 @@ folderLocal.add( props, 'Dip', 0, 90 );
 //       let bottomPointOfIntersection = bottomPlane.intersectLine(yAxisBoundingLines[j]);
 
 //       if (topPointOfIntersection) {
-//         let index = sidesRing.verticies.push(topPointOfIntersection.clone()) - 1;//for some indecipherable reason, when you push to verticies, the function returns the index, plus one.
+//         let index = sidesRing.vertices.push(topPointOfIntersection.clone()) - 1;//for some indecipherable reason, when you push to vertices, the function returns the index, plus one.
 //         topPointsIndexArray.push(index)
 //       } else {
 //         console.log('no top point for:', yAxisBoundingLines[j])
 //       };
 //       if (bottomPointOfIntersection) {
-//         let index = sidesRing.verticies.push(bottomPointOfIntersection.clone())-1;//for some indecipherable reason, when you push to verticies, the function returns the index, plus one.
+//         let index = sidesRing.vertices.push(bottomPointOfIntersection.clone())-1;//for some indecipherable reason, when you push to vertices, the function returns the index, plus one.
 //         bottomPointsIndexArray.push(index)
 //       } else {
 //         console.log('no bottom point for:', yAxisBoundingLines[j])
 //       };
 //     }
     
-//     // loop through comparing verticies and make sides
+//     // loop through comparing vertices and make sides
 //     if (topPointsIndexArray.length == 4 && bottomPointsIndexArray.length == 4) {
 //       for (let j = 0; j < topPointsIndexArray.length; j++) {
 //         for (let k = j + 1; k < topPointsIndexArray.length; k++) {
-//           if ((sidesRing.verticies[topPointsIndexArray[j]].x == sidesRing.verticies[topPointsIndexArray[k]].x) || (sidesRing.verticies[topPointsIndexArray[j]].z == sidesRing.verticies[topPointsIndexArray[k]].z) ) { // if two point are adjacent
+//           if ((sidesRing.vertices[topPointsIndexArray[j]].x == sidesRing.vertices[topPointsIndexArray[k]].x) || (sidesRing.vertices[topPointsIndexArray[j]].z == sidesRing.vertices[topPointsIndexArray[k]].z) ) { // if two point are adjacent
 //             let normal = new THREE.Vector3( 0, 1, 0 )
 //             let face1 = new THREE.Face3( topPointsIndexArray[j], topPointsIndexArray[k], bottomPointsIndexArray[j], normal ) // make face with two points on the top
 //             let face2 = new THREE.Face3( bottomPointsIndexArray[j], bottomPointsIndexArray[k], topPointsIndexArray[k], normal ) // make face with two points on the bottom
@@ -464,31 +479,31 @@ folderLocal.add( props, 'Dip', 0, 90 );
 // }
 
 // Soon obviated
-// function generateClipVerticies() {
-//   let clipVerticies = new THREE.Geometry()
+// function generateClipVertices() {
+//   let clipVertices = new THREE.Geometry()
 //   for (let i = -1; i < 2; i += 2) {
 //     for (let j = -1; j < 2; j += 2) {
 //       for (let k = -1; k < 2; k += 2) {
 //         let n = new THREE.Vector3(i,j,k)
-//         clipVerticies.verticies.push(n.clone())
+//         clipVertices.vertices.push(n.clone())
 //       }
 //     }
 //   }
-//   return clipVerticies
+//   return clipVertices
 // }
 
 // Soon obviated
-// function makeLinesFromVerticies() {
-//   let clipVerticies = generateClipVerticies()
-//   let verticies = clipVerticies.verticies;
-//   for (let i = 0; i < verticies.length; i++) {
-//     for (let j = i+1; j < verticies.length; j++) {
-//       if (verticies[i].x == verticies[j].x && verticies[i].y == verticies[j].y) {
-//         zAxisBoundingLines.push(new THREE.Line3(verticies[i],verticies[j]))
-//       } else if (verticies[i].x == verticies[j].x && verticies[i].z == verticies[j].z) {
-//         yAxisBoundingLines.push(new THREE.Line3(verticies[i],verticies[j]))
-//       } else if (verticies[i].y == verticies[j].y && verticies[i].z == verticies[j].z) {
-//         xAxisBoundingLines.push(new THREE.Line3(verticies[i],verticies[j]))
+// function makeLinesFromVertices() {
+//   let clipVertices = generateClipVertices()
+//   let vertices = clipVertices.vertices;
+//   for (let i = 0; i < vertices.length; i++) {
+//     for (let j = i+1; j < vertices.length; j++) {
+//       if (vertices[i].x == vertices[j].x && vertices[i].y == vertices[j].y) {
+//         zAxisBoundingLines.push(new THREE.Line3(vertices[i],vertices[j]))
+//       } else if (vertices[i].x == vertices[j].x && vertices[i].z == vertices[j].z) {
+//         yAxisBoundingLines.push(new THREE.Line3(vertices[i],vertices[j]))
+//       } else if (vertices[i].y == vertices[j].y && vertices[i].z == vertices[j].z) {
+//         xAxisBoundingLines.push(new THREE.Line3(vertices[i],vertices[j]))
 //       }
 //     }
 //   }
@@ -501,9 +516,9 @@ function planeFromObject(object, faceNumber) {
 
   let mathPlane = new THREE.Plane();
   scene.updateMatrixWorld();
-  let worldA = object.localToWorld(objectPointA.copy(object.geometry.verticies[object.geometry.faces[faceNumber].a]));
-  let worldB = object.localToWorld(objectPointB.copy(object.geometry.verticies[object.geometry.faces[faceNumber].b]));
-  let worldC = object.localToWorld(objectPointC.copy(object.geometry.verticies[object.geometry.faces[faceNumber].c]));
+  let worldA = object.localToWorld(objectPointA.copy(object.geometry.vertices[object.geometry.faces[faceNumber].a]));
+  let worldB = object.localToWorld(objectPointB.copy(object.geometry.vertices[object.geometry.faces[faceNumber].b]));
+  let worldC = object.localToWorld(objectPointC.copy(object.geometry.vertices[object.geometry.faces[faceNumber].c]));
   mathPlane.setFromCoplanarPoints(worldA, worldB, worldC);
   return mathPlane;
 }
